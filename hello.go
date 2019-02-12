0ea48ea592efa304782e02ad375cbadcfd54a502
davincios <jvf.hus@gmail.com>
2019-02-12 21:23:47 +0100

    setting up http server

--- hello.go
+++ hello.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Whoa, Go is neat!")
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Expert web design stuff by davincio")
+}
 
 func main() {
-	x := 15
-	a := &x // memory address
-	fmt.Println(a)
-	fmt.Println(*a)
-	*a = 5
-	fmt.Println(x)
+	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/about/", about_handler)
+	http.ListenAndServe(":8000", nil)
 }
